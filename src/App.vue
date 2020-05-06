53b50c7877e971106b0357f90b4ca649e0f6a6b1
soymikey <soymikey@github.com>
2020-05-06 14:00:10 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    hello
+    <img
+      alt="Vue logo"
+      src="./assets/logo.png"
+    />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
