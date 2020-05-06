bf015902df224ae71b476a94ec7966cffdff5024
soymikey <soymikey@github.com>
2020-05-06 14:35:18 +0800

    add docker

--- src/App.vue
+++ src/App.vue
@@ -14,9 +14,7 @@ import { Component, Vue } from "vue-property-decorator";
 import HelloWorld from "./components/HelloWorld.vue";
 
 @Component({
-  components: {
-    HelloWorld
-  }
+  components: {}
 })
 export default class App extends Vue {}
 </script>
